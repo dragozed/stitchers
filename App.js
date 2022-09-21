b3290e7a5e7121d4a799da6c9f3216a04dea3abb
Hehe xd <alperkaanilik@gmail.com>
2022-09-21 21:07:16 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("TextPressed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden={false} />
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World!!
+      </Text>
+      <Button color="#884158" title="Button" accessibilityLabel="learnmore" />
+      <TouchableWithoutFeedback onPress={() => console.log("imagetapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
